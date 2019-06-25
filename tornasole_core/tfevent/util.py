0c8f296c6d0392ad5454264e00e0c41c06959375
Rahul Huilgol <huilgolr@amazon.com>
2019-06-25 08:51:37 -0700

    fix dtype conversion issues

--- tornasole_core/tfevent/util.py
+++ tornasole_core/tfevent/util.py
@@ -2,36 +2,28 @@ from .tensor_pb2 import TensorProto
 from .tensor_shape_pb2 import TensorShapeProto
 import numpy as np
 
-
+# hash value of ndarray.dtype is not the same as np.float class
+# so we need to convert the type classes below to np.dtype object
 _NP_DATATYPE_TO_PROTO_DATATYPE = {
-    np.float16: "DT_FLOAT16",
-    np.float32:"DT_FLOAT",
-    np.float64:"DT_DOUBLE",
-    #float64:"DT_DOUBLE",
-    np.int32:"DT_INT32",
-    np.int64:"DT_INT64",
-    np.uint8:"DT_UINT8",
-    np.uint16:"DT_UINT16",
-    np.uint32:"DT_UINT32",
-    np.uint64:"DT_UINT64",
-    np.int8:"DT_INT8",
-    np.int16:"DT_INT16",
-    np.complex64:"DT_COMPLEX64",
-    np.complex128:"DT_COMPLEX128",
-    np.bool:"DT_BOOL"
+    np.dtype(np.float16): "DT_FLOAT16",
+    np.dtype(np.float32):"DT_FLOAT",
+    np.dtype(np.float64):"DT_DOUBLE",
+    np.dtype(np.int32):"DT_INT32",
+    np.dtype(np.int64):"DT_INT64",
+    np.dtype(np.uint8):"DT_UINT8",
+    np.dtype(np.uint16):"DT_UINT16",
+    np.dtype(np.uint32):"DT_UINT32",
+    np.dtype(np.uint64):"DT_UINT64",
+    np.dtype(np.int8):"DT_INT8",
+    np.dtype(np.int16):"DT_INT16",
+    np.dtype(np.complex64):"DT_COMPLEX64",
+    np.dtype(np.complex128):"DT_COMPLEX128",
+    np.dtype(np.bool):"DT_BOOL"
 }
 
 def _get_proto_dtype(npdtype):
     if npdtype.kind == 'U':
         return (False, "DT_STRING")
-    if npdtype == np.float64:
-        return (True, "DT_DOUBLE")
-    if npdtype == np.float32:
-        return (True, "DT_FLOAT")
-    if npdtype == np.int32:
-        return (True, "DT_INT32")
-    if npdtype == np.int64:
-        return (True, "DT_INT64")
     return (True, _NP_DATATYPE_TO_PROTO_DATATYPE[npdtype])
 
 def make_tensor_proto(nparray_data, tag):
