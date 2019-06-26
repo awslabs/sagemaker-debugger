c026c98bf62daefa5fe9ae6733fa20c3e062545b
lakshya97 <lakshya.jain@berkeley.edu>
2019-06-25 17:30:15 -0700

    Implementing Asnychronous Handler (#11)
    * fixed the data not done typo
    
    * added documentation!
    
    * s3 handler with prefix filtering added
    
    * fixing merge conflict
    
    * added a lot of prefix/delimiter/startafter options
    
    * more documentation added
    
    * clarified purpose of readobjectrequest in documentation
    
    * cleaned up documentation, added tests in this dir
    
    * fixed error messages
    
    * fixed logger issues

---
+++ tests/test_handler.py
@@ -0,0 +1,104 @@
+#from tornasole_rules.trial import S3Trial
+from tornasole_core.access_layer.s3handler import *
+from tornasole_core.utils import read_tensor_from_record
+######## HELPER CLASSES AND FUNCTIONS #######
+class TensorLocation:
+    def __init__(self, event_file_name, start=0, length=None):
+        self.event_file_name = event_file_name
+        self.start = start
+        self.length = None
+
+class Index():
+    def __init__(self):
+        self.dummy = dict()
+        self.dummy["s3://ljain-tests/tfevents"] = dict()
+        for i in range(5000): 
+            self.dummy["s3://ljain-tests/tfevents"]["demo_" + str(i)] = [(0, TensorLocation("s3://ljain-tests/tfevents/demo_"+str(i)+".out.tfevents"))]
+
+    # input to get_index_for_tensors is a dict {path:{tensornames:[step_nums]}}
+    # output of that fn is dict {path:{tname:[(step_num, TensorLocation)]}}
+    def get_index_for_tensors(self, t_dict):
+        dict_to_return = dict()
+        for key in t_dict.keys():
+            dict_to_return[key] = dict()
+            for tname in t_dict[key]:
+                dict_to_return[key][tname] = self.dummy[key][tname]
+        return dict_to_return
+
+def load_index():
+    return Index()
+
+# tlist should be a list of [(tname, [steps])]. This method will return a 
+# dictionary with key = (tname, step) and value being the corresponding tensor.
+# If the corresponding tensor is not fetchable, then None is stored for its dictionary entry.
+def get_tensors(index, s3_handler, tlist, num_async_calls=500, timer=False):
+    object_requests = []
+    bucket = "ljain-tests"
+    prefix = "tfevents"
+    index_dict = dict()
+    parent_path = "s3://" + bucket + "/" + prefix
+    for name, steps in tlist:
+        index_dict[name] = steps
+    key_lst = []
+    t_index = index.get_index_for_tensors({parent_path:index_dict})
+    for tname in t_index[parent_path].keys():
+        for step, tloc in t_index[parent_path][tname]:
+            path, start, length = tloc.event_file_name, tloc.start, tloc.length
+            req = ReadObjectRequest(path, start, length)
+            object_requests += [req]
+            key_lst += [(tname, step)]
+    raw_data = s3_handler.get_objects(object_requests, num_async_calls, timer=timer)
+    tensors = dict()
+    for i in range(len(raw_data)):
+        data = raw_data[i]
+        if data is None:
+            tensors[key_lst[i]] = None
+            continue
+        # as read_tensor_from_record returns a list containing tensors and as this file only has one tensor
+        # take the first/only element of the list
+        tensors[key_lst[i]] = read_tensor_from_record(data)[0] 
+    return tensors
+
+##########################################################
+## Tests that downloads of objects from S3 handler are working correctly
+## Downloads and checks values of 100 numpy tensors asynchronously from the S3 bucket ljain-tests
+
+def test_download_objects(compare_speeds = False):
+    # s3trial = S3Trial('test', 'ljain-tests', 'demo')
+    index = load_index()
+    s3_handler = S3Handler()
+    tlist = [("demo_" + str(i), 0) for i in range(100)]
+    print("Async...")
+    tensors = get_tensors(index, s3_handler, tlist, timer=True)
+    assert len(tensors.keys()) == 100
+    for tup in tensors.keys(): 
+        tensor = tensors[tup]
+        assert tensor.shape == (300, 300, 2)
+        assert not np.any(np.ones((300,300,2)) - tensor)
+    if compare_speeds:
+        print("Synchronous...")
+        tensors = get_tensors(index, s3_handler, tlist, num_async_calls = 1, timer=True)
+    s3_handler.close_client()
+##########################################################
+## Tests that listing of objects from S3 handler are working correctly
+## Lists files from 4 different directories
+## Also tests the StartAfter functionality and the delimiter and prefix functionality
+
+def test_list_objects():
+    # s3trial = S3Trial('test', 'ljain-tests', 'demo')
+    s3_handler = S3Handler()
+    req1 = ListRequest('ljain-tests', 'tfevents', '', '')
+    req2 = ListRequest('ljain-tests', 'rand_4mb_1000', '', '')
+    req3 = ListRequest('ljain-tests', 'rand_8mb_1000', '', '')
+    req4 = ListRequest('ljain-tests', 'demo_dir_structure/', '/')
+    req5 = ListRequest('ljain-tests', 'demo_dir_structure/', '/', 'demo_dir_structure/help')
+    files = s3_handler.list_prefixes([req1, req2, req3, req4, req5])
+    # test StartAfter and delimiters
+    assert len(files[3]) == 3 and len(files[4]) == 2
+    assert len(files[0]) == 5000
+    assert len(files[1]) == 1001 
+    assert len(files[2]) == 1001
+    s3_handler.close_client()
+    
+test_list_objects()
+test_download_objects(compare_speeds = True)
---
+++ tornasole_core/access_layer/s3handler.py
@@ -0,0 +1,174 @@
+import aioboto3
+import asyncio
+from tornasole_core.utils import is_s3, get_logger
+import logging
+import time
+import numpy as np
+
+# Must be created for ANY file read request, whether from S3 or Local
+# If you wish to download entire file, leave length as None and start as 0.
+# If length is None, start must be 0.
+# Full S3 path is required if you wish to download file from s3
+# eg s3://ljain-tests/demos/ ....
+class ReadObjectRequest:
+    def __init__(self, path, start=0, length=None):
+        self.is_s3, self.bucket, self.path = is_s3(path)
+        if not self.is_s3:
+            self.path = path
+            self.bucket = None
+        assert start >= 0 and (start == 0 or length is not None)
+        self.start = start
+        self.length = length
+        self.download_entire_file = (self.start == 0 and self.length is None)
+
+
+# Only to list files in S3. Accepts strings Bucket, Prefix, Delimiter, and StartAfter 
+# parameters that serve the same role here as they would in boto3.
+class ListRequest:
+    def __init__(self, Bucket, Prefix="", Delimiter="", StartAfter=""):
+        self.bucket = Bucket
+        self.prefix = Prefix
+        self.delimiter = Delimiter
+        self.start_after = StartAfter
+
+class S3Handler:
+    # For debug flag, first set PYTHONASYNCIODEBUG=1 in terminal.
+    # This provides terminal output revealing details about the AsyncIO calls and timings that may be useful.
+    # num_retries: the number of times to retry a download or connection before logging an exception.
+    def __init__(self, num_retries=5, debug=False):
+        self.loop = asyncio.get_event_loop()
+        self.client = aioboto3.client('s3', loop=self.loop)
+        self.num_retries = num_retries
+        self.logger = get_logger()
+        if debug:
+            self.loop.set_debug(True)
+            self.loop.slow_callback_duration = 4
+            logging.basicConfig(level=logging.DEBUG)
+            aioboto3.set_stream_logger(name='boto3', level=logging.DEBUG, format_string=None)
+
+    # Accepts a bucket, prefix, delimiter, and start token 
+    # and returns list of all file names from that bucket that matches the given configuration
+    # E.g. bucket='ljain-tests', prefix="rand_1mb_1000", start_after="rand_1mb_1000/demo_96.out.tfevents"
+    # Would list all files in the subdirectory 'ljain-tests/rand_1mb_1000' with filenames lexicographically 
+    # less than demo_96.out.tfevents.
+    # If request made is invalid, this returns an empty list and logs the exception in the log.
+    # 
+    async def _list_files(self, bucket, prefix="", delimiter="", start_after=""):
+        count = 0
+        success = False
+        # try num_retries times to establish a connection and download the file; if none can be established, log an error and exit
+        while count < self.num_retries and not success:
+            try:
+                paginator = self.client.get_paginator('list_objects_v2')
+                page_iterator = paginator.paginate(Bucket=bucket, Prefix=prefix, Delimiter=delimiter, 
+                                                    StartAfter=start_after, PaginationConfig={'PageSize': 1000})
+                success = True
+            except Exception as e:
+                self.logger.info(str(e))
+            count += 1
+        if not success:
+            self.logger.warn("Unable to list files for " + bucket + " with prefix " + prefix)
+            return []
+        keys = []
+        async for page in page_iterator:
+            if delimiter:
+                try:
+                    for pre in page['CommonPrefixes']:
+                        keys += [pre['Prefix']]
+                except:
+                    msg = "Could not list directories for " + str(bucket) 
+                    if prefix:
+                        msg += " with prefix " + str(prefix) 
+                    msg += " and delimiter " + str(delimiter)
+                    if start_after:
+                        msg += " starting after " + str(start_after)
+                    self.logger.warn(msg)
+            else:
+                try:
+                    for obj in page['Contents']:
+                        key = obj['Key']
+                        keys += [key]
+                except:
+                    msg = "Could not list directories for " + str(bucket) 
+                    if prefix:
+                        msg += " with prefix " + str(prefix) 
+                    if start_after:
+                        msg += " starting after " + str(start_after)
+                    self.logger.warn(msg) 
+        return keys
+
+    # accepts a bucket and key and fetches data from s3 beginning at the offset = start with the provided length
+    # If length is None, download the entire file.
+    async def _get_object(self, bucket, key, start, length):
+        count, body = 0, None
+        while count < self.num_retries and body is None:
+            try:
+                if length is not None:
+                    bytes_range = "bytes=" + str(start) + "-" + str(start + length)
+                    resp = await self.client.get_object(Bucket=bucket, Key=key, bytes_range=bytes_range)
+                else:
+                    resp = await self.client.get_object(Bucket=bucket, Key=key)
+                body = await resp['Body'].read()
+            except Exception as e:
+                self.logger.warn(str(e))
+                self.logger.warn("Unable to read tensor from object " + str(bucket) + "/" 
+                                + str(key) + " with offset " + str(start) + "-" + str(start + length))
+                body = None
+            count += 1
+        if body is None:
+            self.logger.warn("Unable to find file " + key)
+        return body
+
+    # object_requests: a list of ObjectRequest objects.
+    # Returns list of all data fetched.
+    async def _get_objects(self, object_requests):
+        request_params = []
+        for obj in object_requests:
+            request_params += [(obj.bucket, obj.path, obj.start, obj.length)]
+        data = await asyncio.gather(*[self._get_object(bucket, key, start, length) for bucket, key, start, length in request_params])
+        return data
+
+    # list_requests: a list of ListRequest objects 
+    # Returns list of lists of files fetched
+    async def _list_files_from_requests(self, list_requests):
+        request_params = []
+        for req in list_requests:
+            request_params += [(req.bucket, req.prefix, req.delimiter, req.start_after)]
+        data = await asyncio.gather(*[self._list_files(bucket, prefix, delimiter, start_after) 
+                                        for bucket, prefix, delimiter, start_after in request_params])
+        return data
+
+    # Closes the client
+    async def _close_client(self):
+        await self.client.close()
+
+    # object_requests: list of ObjectRequest objects.
+    # num_async_calls: the number of asynchronous calls to do at once. 
+    # timer: boolean, allows timing measurement of total operation
+    # returns list of all data downloaded.
+    def get_objects(self, object_requests, num_async_calls = 500, timer=False):
+        if timer:
+            start = time.time()
+        idx = 0
+        data = []
+        while idx < len(object_requests):
+            task = self.loop.create_task(self._get_objects(object_requests[idx:idx + num_async_calls]))
+            done = self.loop.run_until_complete(task)
+            data += done
+            idx += num_async_calls
+        if timer:
+            self.logger.info("total time taken for " + str(len(object_requests)) + " requests: " + str(time.time() - start))
+        return data 
+
+    # accepts a list of ListRequest objects, returns list of lists of files fetched.
+    def list_prefixes(self, list_requests):
+        task = self.loop.create_task(self._list_files_from_requests(list_requests))
+        done = self.loop.run_until_complete(task)
+        return done 
+
+    # Public facing function to close the client.
+    def close_client(self):
+        task = self.loop.create_task(self._close_client())
+        done = self.loop.run_until_complete(task)
+        return done
+
