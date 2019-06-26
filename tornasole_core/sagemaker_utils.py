18f1e3f81479eac2dd4562c94591002ba9b42187
Vikas Kumar <vikumar@amazon.com>
2019-06-26 13:33:48 -0700

    Tornasole core working with s3 streaming reads
    Some sagemaker actions added
    NoMoreDataError and TensorUnsatisfiable exception added
    Correscting misspelt function name in sagemaker utils

--- tornasole_core/sagemaker_utils.py
+++ tornasole_core/sagemaker_utils.py
@@ -2,7 +2,7 @@ import boto3
 import botocore
 class SageMakerUtils:
     @staticmethod
-    def is_sagemaker_job_finsihed(jobid, returnMock=None):
+    def is_sagemaker_job_finished(jobname, returnMock=None):
         if returnMock is not None:
             return returnMock
         client = boto3.client('sagemaker')
@@ -12,3 +12,21 @@ class SageMakerUtils:
             return False
         elif status in ['Completed', 'Failed', 'Stopped']:
             return True  # return 1 if the job is finished
+
+    @staticmethod
+    def terminate_sagemaker_job(jobname):
+        client = boto3.client('sagemaker')
+        try :
+            client.stop_training_job(TrainingJobName=jobname)
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def add_tags(sm_job_name, tags):
+        client = boto3.client('sagemaker')
+        # TODO create resource arn here
+        resource_arn = 'arn:aws:sagemaker:us-east-1:072677473360:training-job/' + sm_job_name
+        try:
+            client.add_tags(ResourceArn=resource_arn, Tags=tags)
+        except Exception as e:
+            print(e)
