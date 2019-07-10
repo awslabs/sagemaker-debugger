c0abb2e8972118d75565d9bf175b13777b8f3f27
Vikas89 <dev.vikas94@gmail.com>
2019-07-10 22:24:06 +0000

    Making logger module singleton. The problem with existing module was, each get logger call will create another logger and application will go out of file descriptors error.
    Test: Ran train_imagenet script from tornasole_tf ,
    checked output of ls -l /proc/{procid}/fd and
    verified that the fds are not increasing

--- tornasole_core/utils.py
+++ tornasole_core/utils.py
@@ -20,35 +20,40 @@ def flatten(lis):
           new_lis.append(item)
   return new_lis
 
+logger = None
+
 def get_logger(path=os.getcwd()):
-  logger = logging.getLogger("tornasole")
-  fh = logging.FileHandler(os.path.join(path, 'tornasole.log'))
-  log_level = os.environ.get('TORNASOLE_LOG_LEVEL', default='info')
-  log_level = log_level.lower()
+  global logger
+  if logger is None:
+    logger = logging.getLogger("tornasole")
+    fh = logging.FileHandler(os.path.join(path, 'tornasole.log'))
+    log_level = os.environ.get('TORNASOLE_LOG_LEVEL', default='info')
+    log_level = log_level.lower()
 
-  if log_level not in ['info', 'debug', 'warning', 'error', 'critical', 'off']:
-    print('Invalid log level for TORNASOLE_LOG_LEVEL')
-    log_level = 'info'
+    if log_level not in ['info', 'debug', 'warning', 'error', 'critical', 'off']:
+      print('Invalid log level for TORNASOLE_LOG_LEVEL')
+      log_level = 'info'
 
-  if log_level == 'off':
-    logger.disabled = True
-  elif log_level == 'critical':
-    logger.setLevel(logging.CRITICAL)
-  elif log_level == 'error':
-    logger.setLevel(logging.ERROR)
-  elif log_level == 'warning':
-    logger.setLevel(logging.WARNING)
-  elif log_level == 'info':
-    logger.setLevel(logging.INFO)
-  elif log_level == 'debug':
-    logger.setLevel(logging.DEBUG)
+    if log_level == 'off':
+      logger.disabled = True
+    elif log_level == 'critical':
+      logger.setLevel(logging.CRITICAL)
+    elif log_level == 'error':
+      logger.setLevel(logging.ERROR)
+    elif log_level == 'warning':
+      logger.setLevel(logging.WARNING)
+    elif log_level == 'info':
+      logger.setLevel(logging.INFO)
+    elif log_level == 'debug':
+      logger.setLevel(logging.DEBUG)
 
-  fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-  logger.addHandler(fh)
+    fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(fh)
   # logger.propagate = False
-  return logger
+    return logger
+  else:
+    return logger
 
-logger = get_logger()
 
 def get_immediate_subdirectories(a_dir):
   return [name for name in os.listdir(a_dir)
