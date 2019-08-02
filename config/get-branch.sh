4c9358fac74e69a5d24644d873b21c31f13bb7e2
Kaustubh Milindrao Sardar <kaustubhsardar@gmail.com>
2019-08-02 12:24:39 -0700

    fixed an error in get-branch (#51)
    * fixed an error in get-branch
    
    * fixed an error in get-branch

--- config/get-branch.sh
+++ config/get-branch.sh
@@ -14,18 +14,20 @@ if [ "$CODEBUILD_GIT_BRANCH" = "" ] ; then
 fi
 SUBSTRING=$(echo $CODEBUILD_WEBHOOK_BASE_REF| cut -d'/' -f 3)
 BRANCH=''
-if  [ "$CODEBUILD_WEBHOOK_EVENT" = "PULL_REQUEST_CREATED" ] || [ "$CODEBUILD_WEBHOOK_EVENT" = "PULL_REQUEST_REOPENED" ] || [ "$CODEBUILD_WEBHOOK_EVENT" = "PULL_REQUEST_UPDATED" ]; then
+if  [ "$CODEBUILD_WEBHOOK_EVENT" = "PULL_REQUEST_CREATED" ] ||  [ "$CODEBUILD_WEBHOOK_EVENT" = "PULL_REQUEST_REOPENED" ] || [ "$CODEBUILD_WEBHOOK_EVENT" = "PULL_REQUEST_UPDATED" ]  || [ "$CODEBUILD_WEBHOOK_EVENT" = "PULL_REQUEST_MERGED" ] && [ "$CODEBUILD_WEBHOOK_EVENT" != "PUSH" ]; then
       BRANCH=$SUBSTRING
 
-elif [ "$CODEBUILD_WEBHOOK_EVENT" != "PULL_REQUEST_CREATED" ] && [ "$CODEBUILD_WEBHOOK_EVENT" != "PULL_REQUEST_REOPENED" ] && [ "$CODEBUILD_WEBHOOK_EVENT" != "PULL_REQUEST_UPDATED" ] && [ "$CODEBUILD_GIT_BRANCH" != "alpha" ] && [ "$CODEBUILD_GIT_BRANCH" != "master" ] ; then
+elif [ "$CODEBUILD_WEBHOOK_EVENT" != "PULL_REQUEST_CREATED" ] && [ "$CODEBUILD_WEBHOOK_EVENT" != "PULL_REQUEST_REOPENED" ] && [ "$CODEBUILD_WEBHOOK_EVENT" != "PULL_REQUEST_UPDATED" ] && [ "$CODEBUILD_WEBHOOK_EVENT" != "PULL_REQUEST_MERGED" ] && [ "$CODEBUILD_GIT_BRANCH" != "alpha" ] && [ "$CODEBUILD_GIT_BRANCH" != "master" ] ; then
+     cd $CODEBUILD_SRC_DIR && git checkout $CODEBUILD_GIT_BRANCH
      if [ $(git merge-base --is-ancestor $CODEBUILD_GIT_BRANCH  "alpha" ; echo $?) -eq 1 ]; then
           BRANCH='alpha'
 
      elif [ $(git merge-base --is-ancestor $CODEBUILD_GIT_BRANCH  "alpha" ; echo $?) -eq 0 ]; then
           BRANCH='master'
-    
-     fi    
-     
+
+     fi
+     cd ..
+
 else BRANCH=$CODEBUILD_GIT_BRANCH
 fi
 
@@ -36,7 +38,7 @@ MXNET_BRANCH=$BRANCH  ;
 
 
 if [ "$CODEBUILD_GIT_BRANCH" != "alpha" ] && [ "$CODEBUILD_GIT_BRANCH" != "master" ] ; then
-    file="configure_branch_for_test.txt"
+    file="config/configure_branch_for_test.txt"
     while IFS=: read -r repo_name default_or_branchname
     do
                 if [ "$repo_name" = "$tf_repo" ] && [ "$default_or_branchname" != "default" ]; then
@@ -54,51 +56,49 @@ fi
 
 cd $CODEBUILD_SRC_DIR && git checkout $CODEBUILD_GIT_BRANCH
 export CURRENT_COMMIT_HASH=$(git log -1 --pretty=%h);
-export CURRENT_COMMIT_DATE="$(git show -s --format=%ci | cut -d' ' -f 1)_$(git show -s --format=%ci | cut -d' ' -f 2)"; 
+export CURRENT_COMMIT_DATE="$(git show -s --format=%ci | cut -d' ' -f 1)_$(git show -s --format=%ci | cut -d' ' -f 2)";
 export CURRENT_REPO_NAME=$(basename `git rev-parse --show-toplevel`) ;
 export CURRENT_COMMIT_PATH="$CODEBUILD_SRC_DIR/wheels/$CURRENT_COMMIT_DATE/$CURRENT_REPO_NAME/$CURRENT_COMMIT_HASH"
+cd ..
 
 if  [ "$CURRENT_REPO_NAME" != "$core_repo" ]; then
-    cd $CODEBUILD_SRC_DIR_tornasole_core && git checkout $CORE_BRANCH 
+    cd $CODEBUILD_SRC_DIR_tornasole_core && git checkout $CORE_BRANCH
     export CORE_REPO_NAME=$(basename `git rev-parse --show-toplevel`) ;
     export CORE_COMMIT_HASH=$(git log -1 --pretty=%h);
-    export CORE_COMMIT_DATE="$(git show -s --format=%ci | cut -d' ' -f 1)_$(git show -s --format=%ci | cut -d' ' -f 2)"; 
+    export CORE_COMMIT_DATE="$(git show -s --format=%ci | cut -d' ' -f 1)_$(git show -s --format=%ci | cut -d' ' -f 2)";
     export CORE_PATH="$CODEBUILD_SRC_DIR/wheels/$CORE_COMMIT_DATE/$CORE_REPO_NAME/$CORE_COMMIT_HASH"
     cd ..
 fi
 
 if  [ "$CURRENT_REPO_NAME" != "$rules_repo"  ]; then
-    cd $CODEBUILD_SRC_DIR_tornasole_rules && git checkout $RULES_BRANCH 
+    cd $CODEBUILD_SRC_DIR_tornasole_rules && git checkout $RULES_BRANCH
     export RULES_REPO_NAME=$(basename `git rev-parse --show-toplevel`) ;
     export RULES_COMMIT_HASH=$(git log -1 --pretty=%h);
-    export RULES_COMMIT_DATE="$(git show -s --format=%ci | cut -d' ' -f 1)_$(git show -s --format=%ci | cut -d' ' -f 2)"; 
+    export RULES_COMMIT_DATE="$(git show -s --format=%ci | cut -d' ' -f 1)_$(git show -s --format=%ci | cut -d' ' -f 2)";
     export RULES_PATH="$CODEBUILD_SRC_DIR/wheels/$RULES_COMMIT_DATE/$RULES_REPO_NAME/$RULES_COMMIT_HASH"
     cd ..
 fi
 
 if  [ "$CURRENT_REPO_NAME" != "$mxnet_repo" ]; then
-    cd $CODEBUILD_SRC_DIR_tornasole_mxnet && git checkout $MXNET_BRANCH 
+    cd $CODEBUILD_SRC_DIR_tornasole_mxnet && git checkout $MXNET_BRANCH
     export MXNET_REPO_NAME=$(basename `git rev-parse --show-toplevel`) ;
     export MXNET_COMMIT_HASH=$(git log -1 --pretty=%h);
-    export MXNET_COMMIT_DATE="$(git show -s --format=%ci | cut -d' ' -f 1)_$(git show -s --format=%ci | cut -d' ' -f 2)"; 
+    export MXNET_COMMIT_DATE="$(git show -s --format=%ci | cut -d' ' -f 1)_$(git show -s --format=%ci | cut -d' ' -f 2)";
     export MXNET_PATH="$CODEBUILD_SRC_DIR/wheels/$MXNET_COMMIT_DATE/$MXNET_REPO_NAME/$MXNET_COMMIT_HASH"
     cd ..
 fi
 
 if  [ "$CURRENT_REPO_NAME" != "$tf_repo" ]; then
-    cd $CODEBUILD_SRC_DIR_tornasole_tf && git checkout $TF_BRANCH 
+    cd $CODEBUILD_SRC_DIR_tornasole_tf && git checkout $TF_BRANCH
     export TF_REPO_NAME=$(basename `git rev-parse --show-toplevel`) ;
     export TF_COMMIT_HASH=$(git log -1 --pretty=%h);
-    export TF_COMMIT_DATE="$(git show -s --format=%ci | cut -d' ' -f 1)_$(git show -s --format=%ci | cut -d' ' -f 2)"; 
+    export TF_COMMIT_DATE="$(git show -s --format=%ci | cut -d' ' -f 1)_$(git show -s --format=%ci | cut -d' ' -f 2)";
     export TF_PATH="$CODEBUILD_SRC_DIR/wheels/$TF_COMMIT_DATE/$TF_REPO_NAME/$TF_COMMIT_HASH"
     cd ..
 fi
 
-
-            
 export TF_BRANCH ;
 export CORE_BRANCH ;
 export RULES_BRANCH ;
 export MXNET_BRANCH ;
-
 
