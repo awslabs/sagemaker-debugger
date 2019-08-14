aafb747af448e17ab94b124252eddde85978d534
Rahul Huilgol <huilgolr@amazon.com>
2019-08-14 22:56:27 +0000

    comment upload

--- bin/build_binaries.sh
+++ bin/build_binaries.sh
@@ -6,6 +6,6 @@ do
     export TORNASOLE_WITH_$CAPITALIZED_FRAMEWORK=1
     python setup.py bdist_wheel --universal
     unset TORNASOLE_WITH_$CAPITALIZED_FRAMEWORK
-    aws s3 cp dist/tornasole-$VERSION-py2.py3-none-any.whl s3://tornasole-binaries-use1/tornasole_$FRAMEWORK/py3/
+#    aws s3 cp dist/tornasole-$VERSION-py2.py3-none-any.whl s3://tornasole-binaries-use1/tornasole_$FRAMEWORK/py3/
     rm -rf dist build *.egg-info
 done
