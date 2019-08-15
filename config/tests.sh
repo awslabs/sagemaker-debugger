8611d601fa14e9f99da3c016f1038dea2dab1129
Kaustubh Milindrao Sardar <kaustubhsardar@gmail.com>
2019-08-15 11:53:42 -0700

    changes in test.sh (#89)
    * changes
    
    * fixing import error in local trial created by some pr
    
    * fixing import error in local trial created by some pr
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes

--- config/tests.sh
+++ config/tests.sh
@@ -1,8 +1,39 @@
 #!/usr/bin/env bash
+check_logs() {
+  if grep "AssertionError" $1;
+   then
+    echo "Integration tests:FAILED."
+    exit 1
+  else
+        echo "Integration tests: SUCCESS."
+  fi
+}
 
-#export TORNASOLE_LOG_LEVEL=debug
-TORNASOLE_LOG_LEVEL=debug python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/report.html --self-contained-html tests/
-TORNASOLE_LOG_LEVEL=debug python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/test_rules_tensorflow.html --self-contained-html -s tests/analysis/integration_testing_rules.py::test_test_rules --mode tensorflow --path_to_config ./tests/analysis/config.yaml
-TORNASOLE_LOG_LEVEL=debug python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/test_rules_mxnet.html --self-contained-html -s tests/analysis/integration_testing_rules.py::test_test_rules --mode mxnet --path_to_config ./tests/analysis/config.yaml
-TORNASOLE_LOG_LEVEL=debug python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/test_rules_pytorch.html --self-contained-html -s tests/analysis/integration_testing_rules.py::test_test_rules --mode pytorch --path_to_config ./tests/analysis/config.yaml
+set -ex
+export TORNASOLE_LOG_LEVEL=debug
+python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/report_analysis.html --self-contained-html tests/analysis
+python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/report_core.html --self-contained-html tests/core
 
+if [ "$run_pytest_tensorflow" = "enable" ] ; then
+    python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/report_tensorflow.html --self-contained-html tests/tensorflow
+    python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/test_rules_tensorflow.html --self-contained-html -s tests/analysis/integration_testing_rules.py::test_test_rules --mode tensorflow --path_to_config ./tests/analysis/config.yaml
+fi
+
+if [ "$run_pytest_mxnet" = "enable" ] ; then
+    python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/report_mxnet.html --self-contained-html tests/mxnet
+    python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/test_rules_mxnet.html --self-contained-html -s tests/analysis/integration_testing_rules.py::test_test_rules --mode mxnet --path_to_config ./tests/analysis/config.yaml
+fi
+
+if [ "$run_pytest_pytorch" = "enable" ] ; then
+    python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/report_pytorch.html --self-contained-html tests/pytorch
+    python -m pytest --html=upload/$CURRENT_COMMIT_PATH/reports/test_rules_pytorch.html --self-contained-html -s tests/analysis/integration_testing_rules.py::test_test_rules --mode pytorch --path_to_config ./tests/analysis/config.yaml
+fi
+
+check_logs upload/$CURRENT_COMMIT_PATH/reports/report_analysis.html
+check_logs upload/$CURRENT_COMMIT_PATH/reports/report_core.html
+check_logs upload/$CURRENT_COMMIT_PATH/reports/report_tensorflow.html
+check_logs upload/$CURRENT_COMMIT_PATH/reports/report_mxnet.html
+check_logs upload/$CURRENT_COMMIT_PATH/reports/report_pytorch.html
+check_logs upload/$CURRENT_COMMIT_PATH/reports/test_rules_tensorflow.html
+check_logs upload/$CURRENT_COMMIT_PATH/reports/test_rules_mxnet.html
+check_logs upload/$CURRENT_COMMIT_PATH/reports/test_rules_pytorch.html
