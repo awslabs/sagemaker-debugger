9293e0a15f321b7a86336fa1023a072678cc0a9d
Jared T Nielsen <jaredtnielsen@gmail.com>
2019-10-09 12:01:46 -0700

    Revert "Tornasole methods for PT zero-code-change (#271)" (#273)
    This reverts commit ccf6bff7f874ed67bb446e61da0c469d5921795a.

--- tornasole/pytorch/__init__.py
+++ tornasole/pytorch/__init__.py
@@ -4,10 +4,5 @@ from .torch_collection import Collection, CollectionManager
 from .torch_collection import get_collections, get_collection, \
   load_collections,  \
   add_to_collection, add_to_default_collection, reset_collections
-from .zero_code_change import (
-  use_zero_code_change,
-  set_use_zero_code_change,
-  get_zero_code_change_hook,
-)
 from tornasole import SaveConfig, SaveConfigMode, ReductionConfig
 from tornasole import modes
