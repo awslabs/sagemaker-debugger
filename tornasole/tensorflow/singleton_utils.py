ee12466b4f0dd2e7dcf084bddc7b1eb39f2ffead
Jared T Nielsen <jaredtnielsen@gmail.com>
2019-10-28 15:03:29 -0700

    Update get_hook() (#309)

--- tornasole/tensorflow/singleton_utils.py
+++ tornasole/tensorflow/singleton_utils.py
@@ -12,7 +12,12 @@ import tornasole.core.singleton_utils as sutils
 from tornasole.core.singleton_utils import set_hook
 
 
-def get_hook(json_config_path=None) -> "TornasoleHook":
+def get_hook(json_config_path: str = None, hook_type: str = None) -> "TornasoleHook":
+    """
+
+    hook_type is one of ['session', 'estimator', 'keras', None].
+    """
     from tornasole.tensorflow.hook import TornasoleHook
 
+    # TODO(huilgolr): Choose tornasole_hook_type based on `hook_type`.
     return sutils.get_hook(json_config_path=json_config_path, tornasole_hook_class=TornasoleHook)
