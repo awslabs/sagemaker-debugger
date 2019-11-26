b94a7d894fa9b8f32fc96e7c20a273413612b202
Jared Nielsen <jaredtnielsen@gmail.com>
2019-11-26 13:40:40 -0800

    Add test artifacts check to CI

--- config/tests.sh
+++ config/tests.sh
@@ -5,7 +5,7 @@ set -ex
 check_logs() {
   if grep -e "AssertionError" $1;
    then
-    echo "Integration tests:FAILED."
+    echo "Integration tests: FAILED."
     exit 1
   else
         echo "Integration tests: SUCCESS."
@@ -43,3 +43,8 @@ if [ "$run_pytest_pytorch" = "enable" ] ; then
 fi
 
 check_logs $REPORT_DIR/*
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "ERROR: Test artifacts were created. Please place these in /tmp.";
+  echo "$(git status --porcelain)"
+  exit 1
