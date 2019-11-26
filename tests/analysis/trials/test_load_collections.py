66c5d938b284c756fb45f1dded4f4130824b421f
NihalHarish <nihal42harish@gmail.com>
2019-11-26 13:23:51 -0800

    stop waiting if training has ended

---
+++ tests/analysis/trials/test_load_collections.py
@@ -0,0 +1,55 @@
+# Standard Library
+
+# Third Party
+import pytest
+
+# First Party
+from smdebug.trials import create_trial
+
+
+@pytest.mark.slow
+def test_load_collection_files_from_completed_job():
+    """
+    Number of collection files : 2001
+    Training_has_ended.ts : Present
+
+    All the collection files have been written in the test dataset
+    and the training_has_ended file is present
+    :return:
+    """
+    path = "s3://tornasole-testing/collection-tests/all-collection-files-present/"
+    trial = create_trial(path)
+    assert len(trial.workers()) == 2001
+
+
+@pytest.mark.slow
+def test_load_collection_files_from_completed_job_with_missing_files():
+    """
+    Number of collection files : 1446
+    Training_has_ended.ts : Present
+
+    Some of the collection files have been removed in the test dataset.
+    The number of expected collection files is supposed to 2001
+    but the training_has_ended file is present so we stop waiting
+    :return:
+    """
+    path = "s3://tornasole-testing/collection-tests/collection-files-missing/"
+    trial = create_trial(path)
+    assert len(trial.workers()) == 1446
+
+
+@pytest.mark.slow
+def test_load_collection_files_from_incomplete_job():
+    """
+    Number of collection files : 2001
+    Training_has_ended.ts : Absent
+
+    All the collection files have been written in the test dataset
+    and the training_has_ended file is absent
+
+
+    :return:
+    """
+    path = "s3://tornasole-testing/collection-tests/all-collection-files-present-job-incomplete/"
+    trial = create_trial(path)
+    assert len(trial.workers()) == 2001
