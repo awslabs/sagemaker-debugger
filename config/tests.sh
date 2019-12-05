0d2ace5deebdb07a3163bf9d29d5e56cef75a81e
Jared Nielsen <jaredtnielsen@gmail.com>
2019-12-04 22:54:38 -0800

    Verbose output to track down test error

--- config/tests.sh
+++ config/tests.sh
@@ -23,8 +23,8 @@ export SMDEBUG_LOG_LEVEL=info
 
 export OUT_DIR=upload/$CURRENT_COMMIT_PATH
 export REPORT_DIR=$OUT_DIR/pytest_reports
-python -m pytest -W=ignore --html=$REPORT_DIR/report_analysis.html --self-contained-html tests/analysis
-python -m pytest -W=ignore --html=$REPORT_DIR/report_core.html --self-contained-html tests/core
+python -m pytest -v -W=ignore --html=$REPORT_DIR/report_analysis.html --self-contained-html tests/analysis
+python -m pytest -v -W=ignore --html=$REPORT_DIR/report_core.html --self-contained-html tests/core
 
 if [ "$run_pytest_xgboost" = "enable" ] ; then
     run_for_framework xgboost
--- tests/tensorflow/hooks/test_estimator_modes.py
+++ tests/tensorflow/hooks/test_estimator_modes.py
@@ -181,7 +181,7 @@ def test_mnist(out_dir, on_s3=False):
     if on_s3:
         run_id = "trial_" + datetime.now().strftime("%Y%m%d-%H%M%S%f")
         bucket = "smdebug-testing"
-        prefix = "tf/hooks/estimator_modes/" + run_id
+        prefix = "outputs/hooks/estimator_modes/" + run_id
         out_dir = f"s3://{bucket}/{prefix}"
     help_test_mnist(out_dir, save_config=smd.SaveConfig(save_interval=2), num_steps=2, steps=None)
     helper_test_mnist_trial(out_dir)
@@ -224,7 +224,7 @@ def test_mnist_local_multi_save_configs(out_dir, on_s3=False):
     if on_s3:
         run_id = "trial_" + datetime.now().strftime("%Y%m%d-%H%M%S%f")
         bucket = "smdebug-testing"
-        prefix = "tf/hooks/estimator_modes/" + run_id
+        prefix = "outputs/hooks/estimator_modes/" + run_id
         out_dir = f"s3://{bucket}/{prefix}"
     help_test_mnist(
         out_dir,
