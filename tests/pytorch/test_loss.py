ed6f691c3f06304f95b5c1ecd925baf7af756c2c
Vandana Kannan <vandana268@gmail.com>
2019-12-05 15:51:49 -0800

    Fix pt tests

--- tests/pytorch/test_loss.py
+++ tests/pytorch/test_loss.py
@@ -78,8 +78,8 @@ def test_register_loss_functional(out_dir):
     loss_tensor = trial.tensor("nll_loss_output_0")
 
     # Capture ['nll_loss_output_0']
-    assert len(trial.tensor_names()) == 3
-    assert len(loss_coll.tensor_names) == 3
+    assert len(trial.tensor_names()) == 1
+    assert len(loss_coll.tensor_names) == 1
 
     # Loss should be logged for all the steps since passed `available_steps = range(n_steps)`
     assert len(trial.steps()) == n_steps
@@ -103,8 +103,8 @@ def test_register_loss_module(out_dir):
     loss_tensor = trial.tensor("CrossEntropyLoss_output_0")
 
     # Capture ['CrossEntropyLoss_input_0', 'CrossEntropyLoss_input_1', 'CrossEntropyLoss_output_0']
-    assert len(trial.tensor_names()) == 3
-    assert len(loss_coll.tensor_names) == 3
+    assert len(trial.tensor_names()) == 1
+    assert len(loss_coll.tensor_names) == 1
 
     # Loss should be logged for all the steps since passed `available_steps = range(n_steps)`
     assert len(trial.steps()) == n_steps
