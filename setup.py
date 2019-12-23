c35f62e426fbc801493aa68846fd5bb1db28a406
Anirudh Acharya <aanirud@8c85904b0bf4.ant.amazon.com>
2019-12-23 10:06:34 -0800

    grammar

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-""" Amazon SageMaker Debugger is an offering from AWS which help you automate the debugging of machine learning training jobs.
+""" Amazon SageMaker Debugger is an offering from AWS which helps you automate the debugging of machine learning training jobs.
 This library powers Amazon SageMaker Debugger, and helps you develop better, faster and cheaper models by catching common errors quickly.
 It allows you to save tensors from training jobs and makes these tensors available for analysis, all through a flexible and powerful API.
 It supports TensorFlow, PyTorch, MXNet, and XGBoost on Python 3.6+.
 - Zero Script Change experience on SageMaker when using supported versions of SageMaker Framework containers or AWS Deep Learning containers
-- Full visibility into any tensor part of the training process
+- Full visibility into any tensor which is part of the training process
 - Real-time training job monitoring through Rules
 - Automated anomaly detection and state assertions
 - Interactive exploration of saved tensors
