adf9a8bf18c75085107b4a5b7eb5b4c8fd7ab342
Vikas-kum <vikumar@amazon.com>
2019-12-27 20:28:48 -0800

    Fix ci to propogate failures (#118)
    * failing CI if codebuild fails
    
    * Adding duration arg to pytest to find long running tests
    
    * set to fail on pipe failures

--- config/tests.sh
+++ config/tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
 set -ex
+set -o pipefail
 
 check_logs() {
   if grep -e "AssertionError" $1;
@@ -13,7 +15,7 @@ check_logs() {
 }
 
 run_for_framework() {
-    python -m pytest --html=$REPORT_DIR/report_$1.html -v -s --self-contained-html tests/$1
+    python -m pytest --durations=50 --html=$REPORT_DIR/report_$1.html -v -s --self-contained-html tests/$1
 }
 
 export TF_CPP_MIN_LOG_LEVEL=1
@@ -23,8 +25,8 @@ export SMDEBUG_LOG_LEVEL=info
 
 export OUT_DIR=upload/$CURRENT_COMMIT_PATH
 export REPORT_DIR=$OUT_DIR/pytest_reports
-python -m pytest -v -W=ignore --html=$REPORT_DIR/report_analysis.html --self-contained-html tests/analysis
-python -m pytest -v -W=ignore --html=$REPORT_DIR/report_core.html --self-contained-html tests/core
+python -m pytest -v -W=ignore --durations=50 --html=$REPORT_DIR/report_analysis.html --self-contained-html tests/analysis
+python -m pytest -v -W=ignore --durations=50 --html=$REPORT_DIR/report_core.html --self-contained-html tests/core
 
 if [ "$run_pytest_xgboost" = "enable" ] ; then
     run_for_framework xgboost
