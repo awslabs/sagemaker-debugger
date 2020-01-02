fb9d372c2780a2d03e23e6bdd9a9c74613e89a01
Vikas Kumar <vikumar@amazon.com>
2020-01-02 14:35:05 -0800

    fix sh

--- config/check_smdebug_install.sh
+++ config/check_smdebug_install.sh
@@ -5,7 +5,7 @@ python -c "import smdebug"
 res="$?"
 echo "output of import smdebug is: $res"
 
-if [ $1 != "" ]; then
+if [ $1 ]; then
   python -c "import $1"
   res="$?"
   echo "output of import $1 is: $res"
