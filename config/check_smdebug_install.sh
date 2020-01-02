91f1aeb1be6316249274be85a487cdd13a22b71f
Vikas Kumar <vikumar@amazon.com>
2020-01-02 14:29:44 -0800

    changing build spec

--- config/check_smdebug_install.sh
+++ config/check_smdebug_install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -ex
 set -o pipefail
-python -c "import smdebug.tf"
+python -c "import smdebug"
 res="$?"
 echo "output of import smdebug is: $res"
+
+if [ $1 != "" ]; then
+  python -c "import $1"
+  res="$?"
+  echo "output of import $1 is: $res"
+  exit $res
+fi
 exit $res
--- config/tests.sh
+++ config/tests.sh
@@ -15,7 +15,20 @@ check_logs() {
 }
 
 run_for_framework() {
-    python -m pytest --durations=50 --html=$REPORT_DIR/report_$1.html -v -s --self-contained-html tests/$1
+    if [ "$zero_code_change_test" = "enable" ] ; then
+      # ignoring some test becuase they require multiple frmaeworks to be installed, these tests need to be broken down
+      python -m pytest --durations=50 --html=$REPORT_DIR/report_$1.html -v -s --self-contained-html --ignore=tests/core/test_hook_save_scalar.py --ignore=tests/core/test_paths.py --ignore=tests/core/test_index_utils.py --ignore=tests/core/test_collections.py tests/$1
+      if [ "$1" = "mxnet" ] ; then
+        python -m pytest tests/zero_code_change/mxnet_gluon_integration_test.py
+      elif [ "$1" = "pytorch" ] ; then
+        python -m pytest tests/zero_code_change/pytorch_integration_tests.py
+      elif [ "$1" = "tensorflow" ] ; then
+        python -m pytest tests/zero_code_change/tensorflow_integration_tests.py
+      fi
+
+    else
+      python -m pytest --durations=50 --html=$REPORT_DIR/report_$1.html -v -s --self-contained-html tests/$1
+    fi
 }
 
 export TF_CPP_MIN_LOG_LEVEL=1
@@ -26,7 +39,8 @@ export SMDEBUG_LOG_LEVEL=info
 export OUT_DIR=upload/$CURRENT_COMMIT_PATH
 export REPORT_DIR=$OUT_DIR/pytest_reports
 python -m pytest -v -W=ignore --durations=50 --html=$REPORT_DIR/report_analysis.html --self-contained-html tests/analysis
-python -m pytest -v -W=ignore --durations=50 --html=$REPORT_DIR/report_core.html --self-contained-html tests/core
+
+run_for_framework core
 
 if [ "$run_pytest_xgboost" = "enable" ] ; then
     run_for_framework xgboost
