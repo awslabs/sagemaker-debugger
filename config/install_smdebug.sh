36282825c297ea34d55ec73a6fa4908d829db00d
Vikas Kumar <vikumar@amazon.com>
2020-01-02 20:25:30 -0800

    removing force reinstall

--- config/install_smdebug.sh
+++ config/install_smdebug.sh
@@ -8,7 +8,7 @@ cd $CODEBUILD_SRC_DIR
 if [ "$SMDEBUG_S3_BINARY" ]; then
   mkdir -p s3_pip_binary
   aws s3 cp "$SMDEBUG_S3_BINARY" s3_pip_binary
-  pip install --force-reinstall --upgrade s3_pip_binary/*.whl
+  pip install --upgrade s3_pip_binary/*.whl
 else
   python setup.py bdist_wheel --universal && pip install --upgrade --force-reinstall dist/*.whl
 fi
