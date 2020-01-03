ab52e88338cb26d755224e919d7ad8510baa99f2
Vikas Kumar <vikumar@amazon.com>
2020-01-02 17:26:10 -0800

    Fix

--- config/check_smdebug_install.sh
+++ config/check_smdebug_install.sh
@@ -5,10 +5,12 @@ python -c "import smdebug"
 res="$?"
 echo "output of import smdebug is: $res"
 
-version=`python -c "exec(\"import smdebug\nprint(smdebug.__version__)\")`
+#version=python -c "exec(\"import smdebug\nprint(smdebug.__version__)\")`
+version="$(python -c "exec(\"import smdebug\nprint(smdebug.__version__)\")")"
 # force check version, you can set this env variable in build env when releasing
-if [ "$force_check_smdebug_version" != version ]; then
-  echo "force_check_version $force_check_smdebug_version doesn't match version found: $version"
+
+if [ "$force_check_smdebug_version" ]  && [ "$force_check_smdebug_version" != "${version}" ]; then
+  echo "force_check_version $force_check_smdebug_version doesn't match version found: ${version}"
   exit 1
 fi
 
