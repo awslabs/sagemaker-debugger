2a17dc888d01c5d73961ed8c0978116418bc58d7
Anirudh Acharya <aanirud@8c85904b0bf4.ant.amazon.com>
2020-01-15 04:08:08 -0800

    spec

--- config/install_smdebug.sh
+++ config/install_smdebug.sh
@@ -7,7 +7,7 @@ cd $CODEBUILD_SRC_DIR
 # you can provide bip binary as s3 path in the build environment
 if [ "$SMDEBUG_S3_BINARY" ]; then
   mkdir -p s3_pip_binary
-  aws s3 cp "$SMDEBUG_S3_BINARY" s3_pip_binary
+  aws s3 cp --recursive "$SMDEBUG_S3_BINARY" s3_pip_binary
   pip install --upgrade s3_pip_binary/*.whl
 else
   python setup.py bdist_wheel --universal && pip install --upgrade --force-reinstall dist/*.whl
