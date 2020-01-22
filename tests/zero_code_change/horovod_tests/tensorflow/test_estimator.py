95d79b51dc6268872d8236c93815bb45e6f66e4f
NihalHarish <nihal42harish@gmail.com>
2020-01-22 15:49:19 -0800

    init

--- tests/zero_code_change/horovod_tests/tensorflow/test_estimator.py
+++ tests/zero_code_change/horovod_tests/tensorflow/test_estimator.py
@@ -1,11 +1,15 @@
 # Standard Library
 import os
 
+# Third Party
+from tests.zero_code_change.horovod_tests.utils import (
+    build_json,
+    get_available_gpus,
+    launch_horovod_job,
+)
+
 # First Party
 from smdebug.trials import create_trial
-
-# Local
-from .utils import build_json, get_available_gpus, launch_horovod_job
 
 """
 Tested on current DLAMI p3.8xlarge when run from the main directory
