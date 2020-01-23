d2d6ed26eebe8b1f91f5ac10d391ba876dd0e88f
NihalHarish <nihal42harish@gmail.com>
2020-01-22 16:10:44 -0800

    fixes

--- examples/pytorch/sagemaker_official_container/horovod_mnist.py
+++ examples/pytorch/sagemaker_official_container/horovod_mnist.py
@@ -13,6 +13,17 @@ import torch.utils.data.distributed
 from torchvision import datasets, transforms
 
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -103,7 +114,11 @@ if __name__ == "__main__":
         "--epochs", type=int, default=5, metavar="N", help="number of epochs to train (default: 10)"
     )
     parser.add_argument(
-        "--use_only_cpu", action="store_true", default=False, help="disables CUDA training"
+        "--use_only_cpu",
+        action="store_true",
+        type=str2bool,
+        default=False,
+        help="disables CUDA training",
     )
     parser.add_argument(
         "--log-interval",
--- tests/zero_code_change/horovod_tests/utils.py
+++ tests/zero_code_change/horovod_tests/utils.py
@@ -24,12 +24,13 @@ def build_json(out_dir, include_workers="all", include_collections=None, path=No
 
 
 def launch_horovod_job(script_file_path, script_args, num_workers, config_file_path, mode):
-    command = (
-        ["horovodrun", "-np", str(num_workers)] + [sys.executable, script_file_path] + script_args
-    )
+    command = ["mpirun", "-np", str(num_workers)] + [sys.executable, script_file_path] + script_args
     env_dict = os.environ.copy()
     env_dict["SMDEBUG_CONFIG_FILE_PATH"] = f"{config_file_path}"
     env_dict["PYTHONPATH"] = "/home/ubuntu/sagemaker-debugger/"
     if mode == "cpu":
         env_dict["CUDA_VISIBLE_DEVICES"] = "-1"
-    subprocess.check_call(command, env=env_dict)
+    try:
+        subprocess.check_call(command, env=env_dict)
+    except subprocess.CalledProcessError as e:
+        print(e.output)
