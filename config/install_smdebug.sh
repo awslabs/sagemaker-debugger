b8d7498a3b94c26420646aa9ce3b0d47fafc67ed
Anirudh Acharya <aanirud@8c85904b0bf4.ant.amazon.com>
2020-01-23 08:51:59 -0800

    zcc change

--- config/install_smdebug.sh
+++ config/install_smdebug.sh
@@ -3,11 +3,13 @@ set -ex
 set -o pipefail
 
 
+export SMDEBUG_S3_BINARY="s3://smdebug-nightly-binaries/$(date +%F)/"
 cd $CODEBUILD_SRC_DIR
 # you can provide bip binary as s3 path in the build environment
 if [ "$SMDEBUG_S3_BINARY" ]; then
+  echo "Installing smdebug and smdebug_rules from pre-generated pip wheels at $SMDEBUG_S3_BINARY"
   mkdir -p s3_pip_binary
-  aws s3 cp --recursive s3://smdebug-nightly-binaries/$(date +%F)/ s3_pip_binary
+  aws s3 cp --recursive "$SMDEBUG_S3_BINARY" s3_pip_binary
   pip install --upgrade s3_pip_binary/*.whl
 else
   python setup.py bdist_wheel --universal && pip install --upgrade --force-reinstall dist/*.whl
