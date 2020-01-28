6634b54151e58aaf6306094b5578496d8fa06b17
Anirudh Acharya <aanirud@8c85904b0bf4.ant.amazon.com>
2020-01-27 17:03:33 -0800

    comments

--- config/install_smdebug.sh
+++ config/install_smdebug.sh
@@ -2,19 +2,36 @@
 set -ex
 set -o pipefail
 
+CORE_REPO="https://github.com/awslabs/sagemaker-debugger.git"
+RULES_REPO="https://github.com/awslabs/sagemaker-debugger-rules.git"
+SMDEBUG_S3_BINARY="s3://smdebug-nightly-binaries/$(date +%F)/"
 
-export SMDEBUG_S3_BINARY="s3://smdebug-nightly-binaries/$(date +%F)/"
+echo "Cloning sagemaker-debugger and sagemaker-debugger-rules repository."
+cd
+git clone "$CORE_REPO"
+git clone "$RULES_REPO"
+
+export CODEBUILD_SRC_DIR=`pwd`/sagemaker-debugger
+export CODEBUILD_SRC_DIR_RULES=`pwd`/sagemaker-debugger-rules
+
+echo "sagemaker-debugger repository cloned in the path $CODEBUILD_SRC_DIR"
+echo "sagemaker-debugger-rules repository cloned in the path $CODEBUILD_SRC_DIR_RULES"
+
 cd $CODEBUILD_SRC_DIR
-# you can provide bip binary as s3 path in the build environment
-if [ "$SMDEBUG_S3_BINARY" ]; then
-  echo "Installing smdebug and smdebug_rules from pre-generated pip wheels at $SMDEBUG_S3_BINARY"
-  mkdir -p s3_pip_binary
-  aws s3 cp --recursive "$SMDEBUG_S3_BINARY" s3_pip_binary
-  pip install --upgrade s3_pip_binary/*.whl
-else
-  python setup.py bdist_wheel --universal && pip install --upgrade --force-reinstall dist/*.whl
-fi
+echo "Installing smdebug and smdebug_rules from pre-generated pip wheels at $SMDEBUG_S3_BINARY"
+mkdir -p s3_pip_binary
+aws s3 cp --recursive "$SMDEBUG_S3_BINARY" s3_pip_binary
+pip install --upgrade s3_pip_binary/*.whl
 
+CORE_COMMIT=`cat s3_pip_binary/CORE_COMMIT`
+RULES_COMMIT=`cat s3_pip_binary/RULES_COMMIT`
+
+echo "Commit hash on sagemaker-debugger repository being used: $CORE_COMMIT"
+echo "Commit hash on sagemaker-debugger-rules repository being used: $RULES_COMMIT"
+
+cd $CODEBUILD_SRC_DIR_RULES && git checkout "$RULES_COMMIT"
+
+cd $CODEBUILD_SRC_DIR && git checkout "$CORE_COMMIT"
 
 if [ "$run_pytest_mxnet" == 'enable' ]; then
   ./config/check_smdebug_install.sh mxnet
