c6d0c5d8443c500a07c6676e060a50655eeaa3c8
NihalHarish <nihal42harish@gmail.com>
2020-02-17 14:36:37 -0800

    review changes

---
+++ tests/zero_code_change/horovod_tests/constants.py
@@ -0,0 +1,1 @@
+HOROVOD_PYTORCH_TEST_MNIST_SCRIPT = "examples/pytorch/zero_code_change/horovod_mnist.py"
--- tests/zero_code_change/horovod_tests/pytorch/test_distributed_training.py
+++ tests/zero_code_change/horovod_tests/pytorch/test_distributed_training.py
@@ -1,6 +1,7 @@
 # Standard Library
 
 # Third Party
+from tests.zero_code_change.horovod_tests.constants import HOROVOD_PYTORCH_TEST_MNIST_SCRIPT
 from tests.zero_code_change.horovod_tests.utils import build_json, launch_horovod_job
 from torch.cuda import device_count
 
@@ -21,7 +22,7 @@ def basic_test(out_dir, mode):
     if mode == "cpu":
         mode_args += ["--use_only_cpu", "true"]
     launch_horovod_job(
-        script_file_path=f"examples/pytorch/zero_code_change/{HOROVOD_MNIST_SCRIPT_NAME}",
+        script_file_path=HOROVOD_PYTORCH_TEST_MNIST_SCRIPT,
         script_args=mode_args,
         num_workers=num_workers,
         config_file_path=path,
@@ -29,7 +30,6 @@ def basic_test(out_dir, mode):
     )
 
     tr = create_trial(out_dir)
-    print("Saved Tensor Names: {}".format(tr.tensor_names()))
     assert len(tr.workers()) == 1  # We expect only one worker because
     # it has been configured so in HOROVOD_MNIST_SCRIPT_NAME
     assert len(tr.tensor_names()) == 13
@@ -51,7 +51,7 @@ def mode_allworkers(out_dir, mode):
     if mode == "cpu":
         mode_args += ["--use_only_cpu", "true"]
     launch_horovod_job(
-        script_file_path=f"examples/pytorch/zero_code_change/{HOROVOD_MNIST_SCRIPT_NAME}",
+        script_file_path=HOROVOD_PYTORCH_TEST_MNIST_SCRIPT,
         script_args=mode_args,
         num_workers=num_workers,
         config_file_path=path,
@@ -72,15 +72,13 @@ def test_gpu_allworkers(out_dir):
 
 
 def mode_allworkers_saveall(out_dir, mode):
-    path = build_json(
-        out_dir, include_workers="all", save_all=True, include_collections=["weights", "gradients"]
-    )
+    path = build_json(out_dir, include_workers="all", save_all=True)
     num_workers = 1 if bool(device_count()) is False else device_count()
     mode_args = []
     if mode == "cpu":
         mode_args += ["--use_only_cpu", "true"]
     launch_horovod_job(
-        script_file_path=f"examples/pytorch/zero_code_change/{HOROVOD_MNIST_SCRIPT_NAME}",
+        script_file_path=HOROVOD_PYTORCH_TEST_MNIST_SCRIPT,
         script_args=mode_args,
         num_workers=num_workers,
         config_file_path=path,
