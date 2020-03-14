e9605025a6c0c6326785436041b19ceb7a286224
Pedro Larroy <pedro.larroy.lists@gmail.com>
2020-03-13 17:39:09 -0700

    CR

--- compile_protobuf.py
+++ compile_protobuf.py
@@ -27,21 +27,27 @@ def config_logging():
 
 
 def _protoc_bundle():
+    """
+    Returns an archive with the binary protoc distro for the platform
+    """
     import platform
 
     system = platform.system()
     machine = platform.machine()
     if system == "Darwin":
         archive_url = "https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-osx-x86_64.zip"
+        logging.info("Downloading protoc for Darwin: %s", archive_url)
     elif system == "Linux":
         archive_url = f"https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-{machine}.zip"
+        logging.info("Downloading protoc for Linux: %s", archive_url)
     else:
         archive_url = None
     return archive_url
 
 
 def get_protoc():
-    """make sure protoc is available, otherwise download it and return the path to protoc"""
+    """make sure protoc is available, otherwise download it and return a tuple with the protoc
+    binary and a temporary dir if it needed to be downloaded"""
     if not shutil.which("protoc"):
         archive_url = _protoc_bundle()
         if not archive_url:
@@ -55,22 +61,29 @@ def get_protoc():
             zipf.extractall(tmpdir)
         protoc_bin = os.path.join(tmpdir, "bin", "protoc")
         os.chmod(protoc_bin, 0o755)
-        return protoc_bin
-    return shutil.which("protoc")
+        return (protoc_bin, tmpdir)
+    return (shutil.which("protoc"), None)
 
 
 def compile_protobuf():
+    """
+    Compile protobuf files for smdebug
+    """
     logging.info("Compile protobuf")
     logging.info("================")
-    protoc_bin = get_protoc()
+    (protoc_bin, tmpdir) = get_protoc()
     cmd = [protoc_bin]
     proto_files = glob.glob("smdebug/core/tfevent/proto/*.proto")
     cmd.extend(proto_files)
     cmd.append("--python_out=.")
+    logging.info("Call to protoc: %s", " ".join(cmd))
     check_call(cmd)
+    if tmpdir:
+        shutil.rmtree(tmpdir, ignore_errors=True)
 
 
 def main():
+    config_logging()
     compile_protobuf()
     return 0
 
