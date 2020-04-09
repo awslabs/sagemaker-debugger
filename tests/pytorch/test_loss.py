17ec53eca05c203c448775f9978fc98b1e3882dc
NihalHarish <nihal42harish@gmail.com>
2020-04-08 23:35:50 -0700

    lint

--- tests/pytorch/test_loss.py
+++ tests/pytorch/test_loss.py
@@ -43,10 +43,7 @@ def create_net_and_train(out_dir, n_steps, use_loss_module=False, use_loss_funct
     optimizer = optim.SGD(net.parameters(), lr=0.05, momentum=0.9)
     criterion = nn.CrossEntropyLoss()
 
-    hook = Hook(
-        out_dir=out_dir,
-        save_config=SaveConfig(save_interval=1),
-    )
+    hook = Hook(out_dir=out_dir, save_config=SaveConfig(save_interval=1))
     hook.register_module(net)
     if use_loss_module:
         hook.register_loss(criterion)
