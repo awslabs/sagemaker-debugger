abfd28e42ac55b55f73b70e79135c20bfd2ac075
Vikas Kumar <vikumar@amazon.com>
2020-04-12 08:40:24 -0700

    Changing test event_file_retry limit to 5 so that tests can run faster

--- tests/core/test_index_reader.py
+++ tests/core/test_index_reader.py
@@ -2,8 +2,11 @@
 import pytest
 
 # First Party
+from smdebug.core.config_constants import MISSING_EVENT_FILE_RETRY_LIMIT_KEY
 from smdebug.exceptions import TensorUnavailableForStep
 from smdebug.trials import create_trial
+
+os.environ[MISSING_EVENT_FILE_RETRY_LIMIT_KEY] = 5
 
 
 @pytest.mark.slow  # 0:01 to run
