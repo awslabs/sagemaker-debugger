510b315e918ce060f72d689d0db5069b306eb75c
NihalHarish <nihal42harish@gmail.com>
2020-04-17 10:43:16 +0000

    cleanup

--- smdebug/tensorflow/keras.py
+++ smdebug/tensorflow/keras.py
@@ -388,8 +388,6 @@ class KerasHook(TensorflowBaseHook, tf.keras.callbacks.Callback):
 
         if is_tf_version_2x() and tf.executing_eagerly():
             for tensor_ref in self.tensor_refs_to_save_this_step:
-                if tensor_ref.tf_obj is None:
-                    continue
                 tensor = tensor_ref.tf_obj
                 self._save_for_tensor(
                     tensor_name=tensor.name, tensor_value=tensor.value(), check_before_write=False
