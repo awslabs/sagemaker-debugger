5d0ffa31ee9f0ac3881f4a592ba58d875419fc82
NihalHarish <nihal42harish@gmail.com>
2020-04-17 02:59:14 -0700

    revert assert

--- tests/tensorflow2/test_keras_mirrored.py
+++ tests/tensorflow2/test_keras_mirrored.py
@@ -261,7 +261,7 @@ def test_save_all(out_dir, tf_eager_mode):
             + 2 * strategy.num_replicas_in_sync
         )
         # weights, grads, optimizer_variables, metrics, losses, outputs
-    assert len(tr.steps()) == 10
+    assert len(tr.steps()) == 3
 
 
 # Test has become flaky
