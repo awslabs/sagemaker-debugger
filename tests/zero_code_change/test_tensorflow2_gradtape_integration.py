82b6af5a0f09aacb1e667c2f504d188c33338132
Nihal Harish <nihal42harish@gmail.com>
2020-05-19 15:40:02 -0700

    Update test_tensorflow2_gradtape_integration.py

--- tests/zero_code_change/test_tensorflow2_gradtape_integration.py
+++ tests/zero_code_change/test_tensorflow2_gradtape_integration.py
@@ -97,6 +97,8 @@ def helper_test_keras_v2_gradienttape(script_mode: bool = False, json_file_conte
                 print(log)
                 train_acc_metric.reset_states()
             hook = smd.get_hook()
+            if not hook:
+                return # for backwards compatibility 
             assert hook
             hook.close()
             # Check that hook created and tensors saved
