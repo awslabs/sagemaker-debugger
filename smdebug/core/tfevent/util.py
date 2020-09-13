60866923d274d67b401dffb9a4b7be6c8ebe9176
NihalHarish <nihal42harish@gmail.com>
2020-09-13 01:32:17 -0700

    update dtypes only for tf2

--- smdebug/core/tfevent/util.py
+++ smdebug/core/tfevent/util.py
@@ -11,36 +11,40 @@ from .proto.tensor_shape_pb2 import TensorShapeProto
 
 logger = get_logger()
 
-# hash value of ndarray.dtype is not the same as np.float class
-# so we need to convert the type classes below to np.dtype object
-_NP_DATATYPE_TO_PROTO_DATATYPE = {
-    np.dtype(np.float16): "DT_INT32",
-    np.dtype(np.float32): "DT_FLOAT",
-    np.dtype(np.float64): "DT_DOUBLE",
-    np.dtype(np.int32): "DT_INT32",
-    np.dtype(np.int64): "DT_INT64",
-    np.dtype(np.uint8): "DT_UINT8",
-    np.dtype(np.uint16): "DT_UINT16",
-    np.dtype(np.uint32): "DT_UINT32",
-    np.dtype(np.uint64): "DT_UINT64",
-    np.dtype(np.int8): "DT_INT8",
-    np.dtype(np.int16): "DT_INT16",
-    np.dtype(np.complex64): "DT_COMPLEX64",
-    np.dtype(np.complex128): "DT_COMPLEX128",
-    np.dtype(np.bool): "DT_BOOL",
-    np.dtype([("qint8", "i1")]): "DT_QINT8",
-    np.dtype([("quint8", "u1")]): "DT_QUINT8",
-    np.dtype([("qint16", "<i2")]): "DT_QINT16",
-    np.dtype([("quint16", "<u2")]): "DT_UINT16",
-    np.dtype([("qint32", "<i4")]): "DT_INT32",
-}
-
 
 def _get_proto_dtype(npdtype):
+    # hash value of ndarray.dtype is not the same as np.float class
+    # so we need to convert the type classes below to np.dtype object
+    _NP_DATATYPE_TO_PROTO_DATATYPE = {
+        np.dtype(np.float16): "DT_INT32",
+        np.dtype(np.float32): "DT_FLOAT",
+        np.dtype(np.float64): "DT_DOUBLE",
+        np.dtype(np.int32): "DT_INT32",
+        np.dtype(np.int64): "DT_INT64",
+        np.dtype(np.uint8): "DT_UINT8",
+        np.dtype(np.uint16): "DT_UINT16",
+        np.dtype(np.uint32): "DT_UINT32",
+        np.dtype(np.uint64): "DT_UINT64",
+        np.dtype(np.int8): "DT_INT8",
+        np.dtype(np.int16): "DT_INT16",
+        np.dtype(np.complex64): "DT_COMPLEX64",
+        np.dtype(np.complex128): "DT_COMPLEX128",
+        np.dtype(np.bool): "DT_BOOL",
+    }
+
     try:
         from tensorflow import __version__ as tf_version
 
         if version.parse(tf_version) >= version.parse("2.0.0"):
+            _NP_DATATYPE_TO_PROTO_DATATYPE.update(
+                {
+                    np.dtype([("qint8", "i1")]): "DT_QINT8",
+                    np.dtype([("quint8", "u1")]): "DT_QUINT8",
+                    np.dtype([("qint16", "<i2")]): "DT_QINT16",
+                    np.dtype([("quint16", "<u2")]): "DT_UINT16",
+                    np.dtype([("qint32", "<i4")]): "DT_INT32",
+                }
+            )
             from tensorflow.python import _pywrap_bfloat16
 
             # TF 2.x.x Implements a Custom Numpy Datatype for Brain Floating Type
