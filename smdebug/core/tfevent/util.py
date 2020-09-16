2aa662d7dd649a61157b9293e886320851220e82
Nihal Harish <nihal42harish@gmail.com>
2020-09-16 08:41:59 -0700

    Support all tf types (#346)

--- smdebug/core/tfevent/util.py
+++ smdebug/core/tfevent/util.py
@@ -27,13 +27,22 @@ _NP_DATATYPE_TO_PROTO_DATATYPE = {
     np.dtype(np.complex64): "DT_COMPLEX64",
     np.dtype(np.complex128): "DT_COMPLEX128",
     np.dtype(np.bool): "DT_BOOL",
+    np.dtype([("qint8", "i1")]): "DT_QINT8",
+    np.dtype([("quint8", "u1")]): "DT_QUINT8",
+    np.dtype([("qint16", "<i2")]): "DT_QINT16",
+    np.dtype([("quint16", "<u2")]): "DT_UINT16",
+    np.dtype([("qint32", "<i4")]): "DT_INT32",
 }
 
 
 def _get_proto_dtype(npdtype):
-    if npdtype.kind == "U":
-        return (False, "DT_STRING")
-    return (True, _NP_DATATYPE_TO_PROTO_DATATYPE[npdtype])
+    if hasattr(npdtype, "kind"):
+        if npdtype.kind == "U" or npdtype.kind == "O" or npdtype.kind == "S":
+            return False, "DT_STRING"
+    try:
+        return True, _NP_DATATYPE_TO_PROTO_DATATYPE[npdtype]
+    except KeyError:
+        raise TypeError(f"Numpy Datatype: {np.dtype(npdtype)} is currently not supported")
 
 
 def make_tensor_proto(nparray_data, tag):
