1c10565b22b9252efab9284da86dd595b3311dc1
Nihal Harish <nihal42harish@gmail.com>
2020-10-01 18:09:20 -0700

    Pass Variable Length Argument To Old Function Call (#366)

--- tests/tensorflow2/test_model_subclassing.py
+++ tests/tensorflow2/test_model_subclassing.py
@@ -13,6 +13,16 @@ class MyModel(Model):
         self.conv1 = Conv2D(
             32, 3, activation="relu", kernel_initializer=tf.keras.initializers.GlorotNormal(seed=12)
         )
+        self.original_call = self.conv1.call
+
+        def new_call(inputs, *args, **kwargs):
+            # Since we use layer wrapper we need to assert if these parameters
+            # are actually being passed into the original call fn
+            assert kwargs["input_one"] == 1
+            kwargs.pop("input_one")
+            return self.original_call(inputs, *args, **kwargs)
+
+        self.conv1.call = new_call
         self.conv0 = Conv2D(
             32, 3, activation="relu", kernel_initializer=tf.keras.initializers.GlorotNormal(seed=12)
         )
@@ -26,8 +36,7 @@ class MyModel(Model):
     def first(self, x):
         with tf.name_scope("first"):
             tf.print("mymodel.first")
-            x = self.conv1(x)
-            # x = self.bn(x)
+            x = self.conv1(x, input_one=1)
             return self.flatten(x)
 
     def second(self, x):
