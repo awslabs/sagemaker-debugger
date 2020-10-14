05128d97831d1acc8fba1b4139b3db8c98ac4aff
Amol Lele <19983848+leleamol@users.noreply.github.com>
2020-10-13 19:05:33 -0700

    Addressed the review comments to move the installation closer to framework

--- config/tests.sh
+++ config/tests.sh
@@ -60,10 +60,12 @@ if [ "$run_pytest_xgboost" = "enable" ] ; then
 fi
 
 if [ "$run_pytest_tensorflow" = "enable" ] ; then
+    pip install tensorflow_datasets==3.2.1
     run_for_framework tensorflow
 fi
 
 if [ "$run_pytest_tensorflow2" = "enable" ] ; then
+    pip install tensorflow_datasets
     run_for_framework tensorflow2
 fi
 
