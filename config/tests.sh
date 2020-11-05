917eb8e2a03919cf9a06e5adffb77d9f4c18f390
Vikas Kumar <vikumar@amazon.com>
2020-11-05 13:48:50 -0800

    pin tensorflow dataset in test config

--- config/tests.sh
+++ config/tests.sh
@@ -66,7 +66,7 @@ if [ "$run_pytest_tensorflow" = "enable" ] ; then
 fi
 
 if [ "$run_pytest_tensorflow2" = "enable" ] ; then
-    pip install tensorflow_datasets
+    pip install tensorflow_datasets==4.0.1
     run_for_framework tensorflow2
 fi
 
