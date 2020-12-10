5ab10dd2be47d1c93484f0bf6ae9669ff23c7943
NihalHarish <nihal42harish@gmail.com>
2020-12-10 01:11:09 -0800

    save layer input and output

--- smdebug/tensorflow/keras.py
+++ smdebug/tensorflow/keras.py
@@ -588,7 +588,7 @@ class KerasHook(TensorflowBaseHook, tf.keras.callbacks.Callback):
                     self._save_for_tensor(key, logs[key], check_before_write=False)
 
     def _save_layer_input_and_outputs(self):
-        if is_tf_version_2x() is False or (self.tape is False and self.model.run_eagerly is False):
+        if is_tf_version_2x() is False or bool(self.tape) is False:
             return
         for layer_name in self.saved_layers:
             # Save Input
