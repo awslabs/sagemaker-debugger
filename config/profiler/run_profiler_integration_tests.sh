6ede42a3a33c5b73135ef998ee12a51cbae51838
ndodda-amazon <ndodda@amazon.com>
2021-01-27 22:00:34 -0800

    try using env variable

--- config/profiler/run_profiler_integration_tests.sh
+++ config/profiler/run_profiler_integration_tests.sh
@@ -5,8 +5,9 @@
 echo "Start profiler integration tests script."
 
 echo "base ref" $CODEBUILD_WEBHOOK_BASE_REF
+echo $(git diff --name-only master $CODEBUILD_WEBHOOK_BASE_REF 2>/dev/null)
 
-CODEBUILD_GIT_BRANCH=$(git branch | grep '^*' | sed 's/* //' )
+CODEBUILD_GIT_BRANCH$(git branch -q -a --contains HEAD | sed -n 2p | awk '{ printf $1 }' 2>/dev/null)
 echo $CODEBUILD_GIT_BRANCH
 
 disable_integration_tests="true"
