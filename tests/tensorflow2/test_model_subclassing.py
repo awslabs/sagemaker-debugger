9420fd3aaf5f540699f2d8075580cdc4b320bdce
NihalHarish <nihal42harish@gmail.com>
2021-02-12 14:57:16 -0800

    lint

--- tests/tensorflow2/test_model_subclassing.py
+++ tests/tensorflow2/test_model_subclassing.py
@@ -83,5 +83,8 @@ def test_subclassed_model(out_dir):
     if is_tf_2_2():
         # Feature to save model inputs and outputs was first added for TF 2.2.0
         assert trial.tensor_names(collection=smd.CollectionKeys.INPUTS) == ["inputs"]
-        assert trial.tensor_names(collection=smd.CollectionKeys.OUTPUTS) == ["labels", "predictions"]
+        assert trial.tensor_names(collection=smd.CollectionKeys.OUTPUTS) == [
+            "labels",
+            "predictions",
+        ]
         assert len(trial.tensor_names(collection=smd.CollectionKeys.GRADIENTS)) == 6
