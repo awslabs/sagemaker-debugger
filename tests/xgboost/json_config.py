a66afe64bd106f9b423ed2d7f530e65589a92cf2
NihalHarish <nihal42harish@gmail.com>
2021-03-05 13:08:38 -0800

    update tests to cause failure

--- tests/xgboost/json_config.py
+++ tests/xgboost/json_config.py
@@ -6,7 +6,10 @@ def get_json_config(local_path):
     json_config = {
         "S3Path": "s3://kjndjknd_bucket/prefix",
         "LocalPath": local_path,
-        "HookParameters": {"save_all": False, "save_steps": "0,1,2,3"},
+        "HookParameters": {"save_all": False},
+        "CollectionConfigurations": [
+            {"CollectionName": "losses", "CollectionParameters": {"save_interval": 1}}
+        ],
     }
 
     return json.dumps(json_config)
@@ -16,36 +19,9 @@ def get_json_config_full(local_path):
     json_config = {
         "S3Path": "s3://kjndjknd_bucket/prefix",
         "LocalPath": local_path,
-        "HookParameters": {
-            "include_regex": "regexe1,regex2",
-            "reductions": "min,max,mean,std,abs_variance,abs_sum,abs_l2_norm",
-            "include_collections": "collection_obj_name1,collection_obj_name2",
-            "save_all": False,
-            "save_interval": 100,
-            "save_steps": "1,2,3,4",
-            "start_step": 1,
-        },
+        "HookParameters": {"save_all": False},
         "CollectionConfigurations": [
-            {
-                "CollectionName": "collection_obj_name1",
-                "CollectionParameters": {
-                    "include_regex": "regexe5*",
-                    "save_interval": 100,
-                    "save_steps": "1,2,3",
-                    "start_step": 1,
-                    "reductions": "min,abs_max,l1_norm,abs_l2_norm",
-                },
-            },
-            {
-                "CollectionName": "collection_obj_name2",
-                "CollectionParameters": {
-                    "include_regex": "regexe6*",
-                    "save_interval": 100,
-                    "save_steps": "1,2,3",
-                    "start_step": 1,
-                    "reductions": "min,abs_max,l1_norm,abs_l2_norm",
-                },
-            },
+            {"CollectionName": "losses", "CollectionParameters": {"save_interval": 1}}
         ],
     }
     return json.dumps(json_config)
