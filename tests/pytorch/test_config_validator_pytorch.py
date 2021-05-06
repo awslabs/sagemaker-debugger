e695d49b54fa79efa802f5f0d972255183cdffaa
Amol Lele <19983848+leleamol@users.noreply.github.com>
2021-05-05 21:19:14 -0700

    Fixing failure due to monkeypatch

--- tests/pytorch/test_config_validator_pytorch.py
+++ tests/pytorch/test_config_validator_pytorch.py
@@ -8,8 +8,9 @@ from packaging import version
 def test_supported_pytorch_version(monkeypatch):
     import smdebug.pytorch.singleton_utils
 
-    monkeypatch.setattr(smdebug.pytorch.utils, "PT_VERSION", version.parse("1.14"), raising=True)
-    pt_version = smdebug.pytorch.utils.PT_VERSION
-    print(f"Setting the PT_VERSION to {pt_version}")
-    hook = smdebug.pytorch.singleton_utils.get_hook()
-    assert hook == None
+    with monkeypatch.context() as m:
+        m.setattr(smdebug.pytorch.utils, "PT_VERSION", version.parse("1.14"), raising=True)
+        pt_version = smdebug.pytorch.utils.PT_VERSION
+        print(f"Setting the PT_VERSION to {pt_version}")
+        hook = smdebug.pytorch.singleton_utils.get_hook()
+        assert hook == None
--- tests/tensorflow2/test_config_validator_tensorflow.py
+++ tests/tensorflow2/test_config_validator_tensorflow.py
@@ -2,22 +2,14 @@
 from __future__ import print_function
 
 # Third Party
-import pytest
 from packaging import version
-
-
-@pytest.fixture()
-def tensorflow_framework_override(monkeypatch):
-    import smdebug.tensorflow.utils
-
-    monkeypatch.setattr(smdebug.tensorflow.utils, "TF_VERSION", version.parse("1.1"))
-    return
 
 
 def test_supported_tensorflow_version(monkeypatch):
     import smdebug.tensorflow.singleton_utils
     import smdebug.tensorflow.utils
 
-    monkeypatch.setattr(smdebug.tensorflow.utils, "TF_VERSION", version.parse("1.1"), raising=True)
-    hook = smdebug.tensorflow.singleton_utils.get_hook()
-    assert hook == None
+    with monkeypatch.context() as m:
+        m.setattr(smdebug.tensorflow.utils, "TF_VERSION", version.parse("1.1"), raising=True)
+        hook = smdebug.tensorflow.singleton_utils.get_hook()
+        assert hook == None
