6491e7395bd1038600b3a78fc377ae09a0d0c3c5
NihalHarish <nihal42harish@gmail.com>
2021-07-27 16:14:57 -0700

    de-dupe

--- tests/tensorflow2/test_get_hook.py
+++ tests/tensorflow2/test_get_hook.py
@@ -2,27 +2,16 @@
 from statistics import mean
 
 # Third Party
+import pytest
 from tensorflow.python.util.smdebug import get_smdebug_hook
 from tests.utils import SagemakerSimulator, Timer
 
 
-def test_get_smdebug_hook_no_setup(microbenchmark_repeat_constant, microbenchmark_range_constant):
-    times_taken = []
-    for _ in range(microbenchmark_repeat_constant):
-        timer_context = Timer()
-        with timer_context as t:
-            for _ in range(microbenchmark_range_constant):
-                hook = get_smdebug_hook("keras")
-        times_taken.append(t.time_taken)
-
-    mean_time_taken = mean(times_taken)
-    assert mean_time_taken < 15  # current mean = ~14.5 seconds
-
-
+@pytest.mark.parametrize("use_smdebug", ["0", "1"])
 def test_get_smdebug_hook_use_smdebug(
-    microbenchmark_repeat_constant, microbenchmark_range_constant, monkeypatch
+    use_smdebug, microbenchmark_repeat_constant, microbenchmark_range_constant, monkeypatch
 ):
-    monkeypatch.setenv("USE_SMDEBUG", "0")
+    monkeypatch.setenv("USE_SMDEBUG", use_smdebug)
     times_taken = []
     for _ in range(microbenchmark_repeat_constant):
         timer_context = Timer()
@@ -32,7 +21,10 @@ def test_get_smdebug_hook_use_smdebug(
         times_taken.append(t.time_taken)
 
     mean_time_taken = mean(times_taken)
-    assert mean_time_taken < 2  # current mean = ~1.8 seconds
+    if use_smdebug == "0":
+        assert mean_time_taken < 2  # current mean = ~1.8 seconds
+    else:
+        assert mean_time_taken < 15  # current mean = ~14.5 seconds
 
 
 def test_sagemaker_context(microbenchmark_repeat_constant, microbenchmark_range_constant):
