0fa72ed313a9b2d3be2f0bc1622b3c30c95cfcdd
Adam Cherti <adachert@amazon.com>
2022-01-31 16:19:34 -0500

    Compatibility with TF 2.7
    ...by fixing broken tests in test_keras_mirrored.py
    
    Motive: Tensorflow 2.7.0 release
    
    `smdebug.tensorflow.get_hook()` was returning `None`, which made many
    tests fail, but this was actually a false alarm.
    We just have to mark the version of tensorflow we are trying to release (`2.7.0`)
    as a supported framework version, so `get_hook()` doesn't return
    `None'...
    
    I know what you think. It's not pythonic to silently fail and we should
    instead throw an exception, but this code preceded me and I don't want
    to introduce a breaking change (an exception being raised) and I don't
    want to introduce warnings either in case `get_hook()' gets called
    in unexpected ways multiple times during the lifecycle of a training job.
    
    Instead, I just created a tests that alarms us of the situation
    when `get_hook()` might return `None` when it's actually just because we forgot to update a constant.

--- tests/pytorch/test_get_hook.py
+++ tests/pytorch/test_get_hook.py
@@ -1,10 +1,29 @@
-# Standard Library
-# Third Party
 import pytest
+import torch
 from numpy import percentile
 from tests.utils import SagemakerSimulator, Timer, is_running_in_codebuild
 from torch.utils.smdebug import get_smdebug_hook
 
+from smdebug.core.utils import FRAMEWORK, is_framework_version_supported
+
+
+def test_did_you_forget_to_update_the_supported_framework_version():
+    """
+    This test is designed to save you 2 days of debugging time in case you're new
+    to the codebase.
+
+    One of the side-effects of not updating LATEST_SUPPORTED_TF_VERSION with the
+    version of Tensorflow you're trying to release for is that `smdebug.tensorflow.get_hook()` is going to return `None`
+    and many tests will fail.
+
+    This is just to make you aware of the problem.
+    """
+    if not is_framework_version_supported(FRAMEWORK.PYTORCH):
+        raise Exception(
+            "You are running against an unsupported version of Pytorch... apparently."
+            " Please update `smdebug.pytorch.utils.SUPPORTED_TF_VERSION_THRESHOLD`"
+            f" if you are trying to release sagemaker-debugger for the next version of pytorch ({torch.__version__})."
+        )
 
 @pytest.mark.skipif(
     is_running_in_codebuild() is False,
