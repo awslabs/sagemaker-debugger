cb6ec2a130f140cc9d1be2f61ec31868e97f5c5a
zaoliu-aws <101844763+zaoliu-aws@users.noreply.github.com>
2022-05-13 13:26:04 -0700

    add more descriptive message when json reading error happens (#592)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-14-242.ec2.internal>

--- smdebug/profiler/system_profiler_file_parser.py
+++ smdebug/profiler/system_profiler_file_parser.py
@@ -1,6 +1,7 @@
 # First Party
 # Standard Library
 import json
+import os
 
 from smdebug.core.logger import get_logger
 from smdebug.profiler.utils import TimeUnits, convert_utc_timestamp_to_nanoseconds
@@ -51,16 +52,36 @@ class SystemProfilerEventParser:
         try:
             with open(eventfile) as json_data:
                 event_line = json_data.readline()
+                lineNum = 0
                 while event_line:
+                    lineNum += 1
                     json_event = json.loads(event_line)
                     event_line = json_data.readline()
                     self.read_event_from_dict(json_event)
         except Exception as e:
+            with open(eventfile) as j_data:
+                event_line = j_data.readline()
+                lineNumTotal = 0
+                while event_line:
+                    event_line = j_data.readline()
+                    lineNumTotal += 1
+            errStr = 'line No. ' + str(lineNum) + ' in line No.1 to No.' + str(lineNumTotal) + ' lines, has corrupted json format.  '
+            cur_dir = os.path.dirname(eventfile)
+            cur_dir_base = os.path.basename(cur_dir)
+            eventfile_base = os.path.basename(eventfile)
+            parent_dir = os.path.dirname(cur_dir)
+            time_stamp_files = [int(x.split('.')[0]) for x in os.listdir(cur_dir)]
+            time_stamp_dirs = [int(x) for x in os.listdir(parent_dir)]
+            file_pos = time_stamp_files.index(int(eventfile_base.split('.')[0]))
+            dir_pos = time_stamp_dirs.index(int(cur_dir_base))
+            errStr += 'output file '+eventfile_base + ' is file No.' + str(file_pos+1) + ' in file No.1 to No.' 
+            errStr += str(len(time_stamp_files)) +  ', the folder it locates is ' + cur_dir_base + ' which is folder No.' 
+            errStr += str(dir_pos+1) + " in folder No.1 to No." + str(len(time_stamp_dirs)) + "\n"
             self.logger.error(
-                f"Can't open profiler system metric file {eventfile}: Exception {str(e)}"
+                f"Can't open profiler system metric file {eventfile}: Exception {str(e)} \n {errStr}"
             )
             raise ValueError(
-                f"Can't open profiler system metric file {eventfile}: Exception {str(e)}"
+                f"Can't open profiler system metric file {eventfile}: Exception {str(e)} \n {errStr}"
             )
 
     def read_events_from_json_data(self, system_profiler_json_data):
