ccc802c0c19170f7b1142221ec6d5af797ab73f2
atqy <95724753+atqy@users.noreply.github.com>
2022-08-16 10:26:28 -0700

    Add unified RTD search to RTD website (#610)
    * add unified search to RTD website
    
    * configure rtd build environment to make it functional

--- docs/conf.py
+++ docs/conf.py
@@ -93,7 +93,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_css_files = ["css/custom.css"]
+html_css_files = ["css/custom.css", 'pagination.css', 'search_accessories.css']
 
 suppress_warnings = ["autosectionlabel.*"]
 
